d4ef3aab400d568e2ff399ab7558c6c42a96b524
dead-entropy <34753908+dead-entropy@users.noreply.github.com>
2019-06-18 06:59:40 +0100

    Add files via upload

---
+++ R_dates.r
@@ -0,0 +1,14 @@
+
+########## DATES ##############
+
+thor <- c('1978-06-09')
+loki <- c('1989-06-06')
+agegap <- thor - loki
+
+thor <- as.Date(c('1978-06-09'))
+loki <- as.Date(c('1989-06-06'))  
+  
+agegap <- thor - loki
+agegap
+
+# 
